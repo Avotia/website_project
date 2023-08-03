0cb055b440de0f5cb365e72a69321412f70209f7
fvotia <fvotia@yandex.ru>
2023-08-03 18:31:20 +0300

    Сделала так, чтобы в админке отображался блок «объявления» вместо «app_advertisements> и что если объявление создано сегодня – выводится надпись [Сегодня в <время»]

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Advertisement
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'created_date', 'updated_date', 'auction']
+    list_filter = ['auction']
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
 
 # Register your models here.
--- app_advertisements/apps.py
+++ app_advertisements/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
+VERBOSE_APP_NAME = 'Объявления'
 
 class AppAdvertisementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_advertisements'
+    verbose_name = VERBOSE_APP_NAME
