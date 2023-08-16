45dd0fa43017565794268234b6e3ef98ab8bbe9f
fvotia <fvotia@yandex.ru>
2023-08-16 17:50:38 +0300

    Сделала так, чтобы в панели администратора отображалась не ссылка на картинку, а уменьшенная копия самой картинки, и чтобы у объявлений, у которых нет картинки, отображалась статическая картинка по умолчанию

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
@@ -11,6 +14,9 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to="advertisements/")
+
     class Meta:
         db_table = "advertisements"
     def __str__(self):
@@ -35,3 +41,8 @@ class Advertisement(models.Model):
             )
         else:
             return self.updated_at.strftime("%d.%m.%Y в %H:%M")
+
+    @admin.display(description='Изображение')
+    def miniature_image(self):
+        if self.image:
+            return format_html('<img src="{}" style="width: 130px; height: 100px"/>'.format(self.image.url))
